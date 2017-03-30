eca0c19bf0e81f64e747ef54f97dbe2284e158c7
Stefan Horst <stefan.horst@grandcentrix.net>
2017-03-30 09:39:00 +0200

    added todo script

---
+++ todo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+pushd ~/Library/Calendars > /dev/null
+
+if [ -e /tmp/todo ]; then
+	filenames=$(find . -newer /tmp/todo -exec grep -rlI VTODO {} \+)
+	if [ -n $filenames ]; then
+		cat /tmp/todo
+		exit
+	fi
+
+	# empty todo file
+	rm /tmp/todo
+fi
+
+filenames=$(grep -rlI VTODO *)
+while read -r filename; do
+	if grep -q "STATUS:COMPLETED" $filename; then
+		continue
+	fi
+	summary=$(grep "SUMMARY:" $filename | cut -d : -f 2-)
+
+
+	info=$(dirname $(dirname $filename))/info.plist
+	title=$(defaults read ~/Library/Calendars/$info Title)
+
+	echo "* $title: $summary" >> /tmp/todo
+
+done <<< "$filenames"
+
+# output todo file
+cat /tmp/todo
+popd > /dev/null
